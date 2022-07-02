236b345742aabbe9fb5901ce1ed00cce3efb68fc
slahmer97 <sidahmedlahmer@gmail.com>
2022-07-02 17:17:44 +0200

    added video

--- src/Components/ServerStats.js
+++ src/Components/ServerStats.js
@@ -1,16 +1,29 @@
 
 import React from 'react'
 
+import sample from '../travis.mp4';
+
+
+
+
 function joinServer() {
     window.location.href = "fivem://connect/142.132.154.42:30147";
 }
 
 function ServerStatsForm() {
     return (
-        <div>
-            <button onClick={joinServer}>Default</button>;
-            <h1>statistics:</h1>
-            <h2>online players: <span>12/100</span></h2>
+        <div id="sts">
+            <video className='videoTag' autoPlay loop id="myVideo">
+                <source src={sample} type='video/mp4' />
+            </video>
+            <div className="content">
+                <h1>OG's Street</h1>
+                <center>
+                <button id="myBtn" onClick="">Pause</button>
+                <button id="myBtn" onClick="">Whitelist</button>
+                <button id="myBtn" onClick={joinServer}>Join</button>
+                </center>
+            </div>
         </div>
     )
 }
