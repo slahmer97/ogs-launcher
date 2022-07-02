0bad505b884039db7181f9355cdc2ee42579ea06
slahmer97 <sidahmedlahmer@gmail.com>
2022-07-02 16:19:06 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react'
+import LoginForm from "./Components/LoginForm";
+import ServerStatsForm from "./Components/ServerStats";
 function App() {
+  const adminUser = {
+      username: "slahmer",
+      password: "fivem123"
+  }
+
+
+  const [user, setUser] = useState({username:"", password:""});
+  const [error, setError] = useState("")
+  const Login = details => {
+      console.log(details);
+      if (details.username === adminUser.username && details.password === adminUser.password){
+          setUser({
+              username: details.username,
+              password: details.password
+          })
+      }
+      else{
+          setError("details do not match!")
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          {(user.username !== "") ? (
+              <div className="welcome">
+                  <h2>Welcome, <span> {user.username}</span></h2>
+                  <ServerStatsForm/>
+              </div>
+          ):
+          (
+              <LoginForm Login={Login} error={error}/>
+          )
+
+          }
+      </div>
   );
 }
 
---
+++ src/Components/ServerStats.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+
+function ServerStatsForm() {
+    return (
+        <div>
+            <h1>statistics:</h1>
+            <h2>online players: <span>12/100</span></h2>
+        </div>
+    )
+}
+
+export default ServerStatsForm
